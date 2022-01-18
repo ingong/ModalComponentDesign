35d88a526605bb5563d162a6958662ba767c3ad2
ingong <insong1216@gmail.com>
2022-01-19 00:25:42 +0900

    feat: ModalContext, ModalProvider

---
+++ src/ModalProvider.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { ModalsDispatchContext, ModalsStateContext } from './ModalsContext';
+
+const ModalsProvider = ({ children }) => {
+  const [openedModals, setOpenedModals] = useState([]);
+  const open = (Component, props) => {
+    setOpenedModals(modals => {
+      return [...modals, { Component, props }];
+    });
+    setTimeout(() => {
+      console.log(openedModals);
+    }, 1000);
+  };
+
+  const close = Component => {
+    setOpenedModals(modals => {
+      return modals.filter(modal => {
+        return modal.Component !== Component;
+      });
+    });
+  };
+
+  const dispatch = { open, close };
+
+  return (
+    <ModalsStateContext.Provider value={openedModals}>
+      <ModalsDispatchContext.Provider value={dispatch}>{children}</ModalsDispatchContext.Provider>
+    </ModalsStateContext.Provider>
+  );
+};
+
+export default ModalsProvider;
